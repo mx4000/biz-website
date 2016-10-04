f3ac7b74c9ef1732372326dcdbbb48326cadf998
NeoZhangTCL <ztuochao@uwo.ca>
2016-10-04 15:02:43 -0400

    show image

--- js/image.js
+++ js/image.js
@@ -6,12 +6,13 @@ $(function () {
              var scrollTop = ($('html').scrollTop()) ? $('html').scrollTop() : $('body').scrollTop(); // Works for Chrome, Firefox, IE...
              $('html').addClass('noscroll').css('top',-scrollTop);
         }
-        console.log($(this).firstChild);
+        var Imagepath = $(this).context.children[0].src;
+        $('.showImage').attr("src",Imagepath);
     });
 
     $('#show').on('click keyup', function(event) {
         if (event.target == this || event.keyCode == 27) {
-            $(this).removeClass('open');
+            $('#show').removeClass('open');
             var scrollTop = parseInt($('html').css('top'));
             $('html').removeClass('noscroll');
             $('html,body').scrollTop(-scrollTop);
